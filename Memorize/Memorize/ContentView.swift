8c427d9b6dbe2a30c67b17e294561a02556ab783
Ana Geórgia <anageorgiagp@gmail.com>
2021-05-06 02:57:25 -0300

    Card view added

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,10 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUP: false)
+            }
+        }
             .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
     }
 }
+
+struct CardView: View {
+    var isFaceUP: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUP {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white) // Game background
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0) // Card background
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
